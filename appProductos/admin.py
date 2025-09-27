62c6159ed7e430301f4cc2f1385e6f0101c788e9
Isma <ismaarboleda2006@gmail.com>
2025-09-27 14:56:59 -0500

    Initial commit

---
+++ appProductos/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Producto
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre','precio', 'stock', 'categoria')
+    search_fields = ('nombre', 'categoria')
+    list_filter = ('categoria',)
+admin.site.register(Producto, ProductoAdmin)
