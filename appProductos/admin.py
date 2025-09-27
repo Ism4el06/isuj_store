1bf952487558b9e03f349c25b6353eee33392fd8
Isma <ismaarboleda2006@gmail.com>
2025-09-27 15:52:29 -0500

    pecausa

--- appProductos/admin.py
+++ appProductos/admin.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Producto
+from .models import Categoria
 class ProductoAdmin(admin.ModelAdmin):
     list_display = ('nombre','precio', 'stock', 'categoria')
     search_fields = ('nombre', 'categoria')
     list_filter = ('categoria',)
 admin.site.register(Producto, ProductoAdmin)
 admin.site.register(Categoria)
-from .models import Categoria
--- appProductos/models.py
+++ appProductos/models.py
@@ -1,19 +1,23 @@
 from django.db import models
 
 # Create your models here.
-
 class Categoria(models.Model):
     nombre = models.CharField(max_length=100)
 
-    def __str__(self):
+    def _str_(self):
         return self.nombre
-    
+
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
-    categoria = models.CharField(max_length=100)
-    def __str__(self):
+    imagen = models.ImageField (upload_to='productos/', null=True, blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+
+
+
+    def _str_(self):
         return self.nombre
